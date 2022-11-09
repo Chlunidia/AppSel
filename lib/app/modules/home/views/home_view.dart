b186c50e08bab54cadf99023b6979ec54cd6b280
Chlunidia Putri Kieswendha <chlunidia.putri.ft21@mail.umy.ac.id>
2022-11-09 22:03:00 +0700

    Menambahkan class ClipPath

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -13,12 +13,37 @@ class HomeView extends GetView<HomeController> {
         title: const Text('HomeView'),
         centerTitle: true,
       ),
-      body: Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      body: Stack(
+        children: [
+          ClipPath(
+            clipper: ClipPathClass(),
+            child: Container(height: 250,
+            width: Get.width,
+            color: Color(0xFFEC2028),),
+          )
+        ],
       ),
     );
   }
+}
+
+class ClipPathClass extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    path.lineTo(0, size.height - 60);
+    path.quadraticBezierTo(
+      size.width / 2,
+      size.height,
+      size.width,
+      size.height - 60,
+    );
+    path.lineTo(size.width, 0);
+    path.close();
+
+    return path;
+  }
+  
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) => false;
 }
